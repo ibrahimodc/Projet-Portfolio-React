e3201bcf73863b50e5b0c1b42fa09bf89c9d70a8
Ibrahima Diallo <ibrahim.ibn.hi@gmail.com>
2026-04-19 20:07:16 +0000

    Etant donné qu'on a trailler en groupe je laisse la Team au nombre de 5

--- public/components/Team.jsx
+++ public/components/Team.jsx
@@ -1,1 +1,37 @@
 import '../styles/Team.css';
+
+function Team() {
+    const membres = [
+    {
+      nom: "Anna Keita",
+      role: "Sys",
+      photo: "/assets/team/anna-keita.jpeg",
+      description: "Spécialiste en systèmes d'information"
+    },
+    {
+      nom: "Ibrahim DIALLO",
+      role: "SysAdmin",
+      photo: "/assets/team/ibrahim-diallo.jpg",
+      description: "Administrateur système expérimenté"
+    },
+    {
+      nom: "Bamba Lab",
+      role: "Systeme & Reseaux",
+      photo: "/assets/team/bamba-lab.jpg",
+      description: "Expert en systèmes et réseaux"
+    },
+    {
+      nom: "Khadija PENE",
+      role: "Chef de projet",
+      photo: "/assets/team/khadija-pene.jpg",
+      description: "Chef de projet passionnée"
+    },
+    {
+      nom: "Aliou SOW",
+      role: "Developpeur",
+      photo: "/assets/team/aliou-sow.jpg",
+      description: "Développeur full-stack"
+    }
+  ];
+
+}
