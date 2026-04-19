a551a13e280e39a4dabd39ff634085b0904151a1
Ibrahima Diallo <ibrahim.ibn.hi@gmail.com>
2026-04-19 16:50:58 +0000

    Ajout des champs pour la mise en place des clef pour ajouter des projets

--- public/components/AjouterProjet.jsx
+++ public/components/AjouterProjet.jsx
@@ -1,2 +1,10 @@
 import React, { useState } from 'react';
 import { compresserImage } from '../utils/imageUtils';
+
+const CHAMPS_VIDES = {
+    libelle: '',
+    image: '',
+    description: '',
+    technologies: '',
+    lien: '',
+}
