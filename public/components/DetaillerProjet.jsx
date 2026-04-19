db6cd531018e0f1435ae4a075119c10d4bbceb5a
Ibrahima Diallo <ibrahim.ibn.hi@gmail.com>
2026-04-19 18:53:25 +0000

    Cette partie prend lorsqu'aucun projet n'existe il affiche une image en arrière plan

--- public/components/DetaillerProjet.jsx
+++ public/components/DetaillerProjet.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
 function DetaillerProjet({ projet, onFermer }) {
-    
+    const [imgError, setImgError] = useState(false);
+    const placeholder = `https://placehold.co/1200x600/1a1a2e/6c63ff?text=${encodeURIComponent(projet.libelle)}`;
 }
